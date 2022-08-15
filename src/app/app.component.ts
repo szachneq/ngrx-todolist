fea65bf682fade10429a49416f102ea50caaa6c6
Jan Szachno <j.szachno@gmail.com>
2022-08-15 16:05:14 +0200

    added internationalization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
-import { Tasks } from './state/tasks/task.model';
-import * as TaskActions from './state/tasks/task.actions';
 import { AppState } from './state/state';
+import * as TaskActions from './state/tasks/task.actions';
+import { Tasks } from './state/tasks/task.model';
 
 @Component({
   selector: 'my-app',
@@ -13,11 +14,17 @@ import { AppState } from './state/state';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // $localize`:meaning|description@@id:source message text`;
+  // $localize`:meaning|:source message text`;
+  // $localize`:description:source message text`;
+  // $localize`:@@id:source message text`;
+  appName: string = $localize`:appname|the name of the application:Todo List`;
   tasks$: Observable<Tasks>;
   inputValue: string = '';
 
-  constructor(private store: Store<AppState>) {
+  constructor(private titleService: Title, private store: Store<AppState>) {
     this.tasks$ = this.store.select('tasks');
+    this.titleService.setTitle($localize`${this.appName}`);
   }
 
   getTasks() {
